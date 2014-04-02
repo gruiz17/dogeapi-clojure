a0ed4aa7b8379e8d612105b5ffed77073efc3aa4
gruiz17 <gruiz17@berkeley.edu>
2014-04-02 02:06:04 -0700

    moar tests

--- test/dogeapi_clojure/core_test.clj
+++ test/dogeapi_clojure/core_test.clj
@@ -14,11 +14,13 @@
   (def api-url2 "https://www.dogeapi.com/wow/v2/?api_key=1e6fn9y1aik37y47buhqjj2qym")
 
   (is (= (:body (http/get (str api-url "&a=get_balance"))) (dogeapi-clojure.core/get-balance wow)))
+  (is (= (:body (http/get (str api-url "&a=get_my_addresses"))) (dogeapi-clojure.core/get-my-addresses wow)))
   (is (= (:body (http/get (str base-url "?a=get_current_block"))) (dogeapi-clojure.core/get-current-block wow)))
   (is (= (:body (http/get (str base-url "?a=get_difficulty"))) (dogeapi-clojure.core/get-difficulty wow)))
   (is (= (:body (http/get (str base-url "?a=get_current_price"))) (dogeapi-clojure.core/get-current-price wow)))
 
   (is (= (json/read-json (:body (http/get (str api-url2 "&a=get_balance")))) (dogeapi-clojure.core/get-balance wow2)))
+  (is (= (json/read-json (:body (http/get (str api-url2 "&a=get_my_addresses")))) (dogeapi-clojure.core/get-my-addresses wow2)))
   (is (= (json/read-json (:body (http/get (str base-url2 "?a=get_current_block")))) (dogeapi-clojure.core/get-current-block wow2)))
   (is (= (json/read-json (:body (http/get (str base-url2 "?a=get_difficulty")))) (dogeapi-clojure.core/get-difficulty wow2)))
   (is (= (json/read-json (:body (http/get (str base-url2 "?a=get_current_price")))) (dogeapi-clojure.core/get-current-price wow2)))
